e38e7e42d892ac6f6f5693a9b5e2275740d60784
SantiagoBorghi <santiborghi02@gmail.com>
2024-11-15 01:35:51 -0300

    First Commit

---
+++ src/app/core/pages/subscription-options/subscription-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './subscription-options.component.html',
+  styleUrl: './subscription-options.component.scss'
+})
+export class SubscriptionOptionsComponent {
+
+}
